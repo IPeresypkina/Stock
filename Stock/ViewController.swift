f73d5ba71b6b207d46d0a1f703db39679fe7fd6f
IPeresypkina <iperesypkina9@gmail.com>
2020-08-30 01:53:11 +0300

    Initial Commit

---
+++ Stock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stock
+//
+//  Created by Ирина Пересыпкина on 8/30/20.
+//  Copyright © 2020 Ирина Пересыпкина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
